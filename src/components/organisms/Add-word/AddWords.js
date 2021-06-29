f1df3fe35e9cc5ba54fac4dd6afa6ce7030114d1
Szymii <szymimelzer9@gmail.com>
2021-06-29 13:03:28 +0200

    info about errors in modal

--- src/components/organisms/Add-word/AddWords.js
+++ src/components/organisms/Add-word/AddWords.js
@@ -1,7 +1,8 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Form, Title } from './AddWords.styles';
 import FormField from '../../molecules/FormField/FormField';
 import IconBtn from '../../atoms/IconBtn/IconBtn';
+import InfoModal from '../../molecules/InfoModal/InfoModal';
 import { Input } from '../../atoms/Input/Input';
 import { ConfirmBtn } from '../../atoms/ConfirmBtn/ConfirmBtn';
 import { StorageContext } from '../../../StorageProvider';
@@ -10,15 +11,10 @@ import { FaTrash as TrashIcon } from 'react-icons/fa';
 import { FaPlus } from 'react-icons/fa';
 
 const AddWords = () => {
-  const { meaning, setMeaning, word, setWord, updateLocal } = useContext(
-    StorageContext
-  );
-
-  const handleMeaningChange = (e, i) => {
-    const value = [...meaning];
-    value[i] = e.target.value;
-    setMeaning(value);
-  };
+  const { meaning, setMeaning, word, setWord, updateLocal } =
+    useContext(StorageContext);
+  const [isOpen, setIsOpen] = useState(false);
+  const [info, setInfo] = useState('');
 
   const validate = (word, meaning) => {
     const regex = /(.|\s)*\S/;
@@ -27,18 +23,14 @@ const AddWords = () => {
     return true;
   };
 
-  const handleWordChange = (e) => {
-    setWord(e.target.value);
+  const handleMeaningChange = (e, i) => {
+    const value = [...meaning];
+    value[i] = e.target.value;
+    setMeaning(value);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (validate(word, meaning)) {
-      localStorage.setItem(word, JSON.stringify(meaning));
-      updateLocal();
-      setMeaning(['']);
-      setWord('');
-    }
+  const handleWordChange = (e) => {
+    setWord(e.target.value);
   };
 
   const handleAdd = () => {
@@ -55,39 +47,84 @@ const AddWords = () => {
       value.splice(i, 1);
       setMeaning(value);
     }
+    if (meaning.length === 1 && meaning[0] !== '') {
+      const value = [...meaning];
+      value[i] = '';
+      setMeaning(value);
+    }
+  };
+
+  const openModal = (prop) => {
+    if (prop) {
+      setInfo('Successfully added!');
+      setIsOpen(true);
+    } else {
+      setInfo('All field are required.');
+      setIsOpen(true);
+    }
+  };
+
+  useEffect(() => {
+    let isMounted = true;
+    if (isMounted)
+      setTimeout(() => {
+        setIsOpen(false);
+      }, 3500);
+
+    return () => {
+      isMounted = false;
+    };
+  }, [isOpen]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validate(word, meaning)) {
+      localStorage.setItem(word, JSON.stringify(meaning));
+      updateLocal();
+      setMeaning(['']);
+      setWord('');
+      openModal(true);
+    } else {
+      openModal(false);
+    }
   };
 
   return (
-    <Form as="form" autoComplete="off" onSubmit={handleSubmit}>
-      <Title>Your new word:</Title>
-      <FormField>
-        <Input
-          type="text"
-          value={word}
-          name="word"
-          onChange={handleWordChange}
-        />
-      </FormField>
-
-      <Title>Meaning:</Title>
-      {meaning.map((value, i) => (
-        <FormField key={i}>
+    <>
+      <Form as="form" autoComplete="off" onSubmit={handleSubmit}>
+        <Title>Your new word:</Title>
+        <FormField>
           <Input
             type="text"
-            name="meaning"
-            value={value}
-            onChange={(e) => handleMeaningChange(e, i)}
+            value={word}
+            name="word"
+            onChange={handleWordChange}
           />
-          <IconBtn type="button" onClick={handleDelete(i)}>
-            <TrashIcon />
-          </IconBtn>
-          <IconBtn type="button" onClick={handleAdd}>
-            <FaPlus />
-          </IconBtn>
         </FormField>
-      ))}
-      <ConfirmBtn type="submit">Confirm</ConfirmBtn>
-    </Form>
+
+        <Title>Meaning:</Title>
+        {meaning.map((value, i) => (
+          <FormField key={i}>
+            <Input
+              type="text"
+              name="meaning"
+              value={value}
+              onChange={(e) => handleMeaningChange(e, i)}
+            />
+            <IconBtn type="button" onClick={handleDelete(i)}>
+              <TrashIcon />
+            </IconBtn>
+            <IconBtn type="button" onClick={handleAdd}>
+              <FaPlus />
+            </IconBtn>
+          </FormField>
+        ))}
+        <ConfirmBtn type="submit">Confirm</ConfirmBtn>
+      </Form>
+      <InfoModal isOpen={isOpen} handleModalClose={() => setIsOpen(false)}>
+        {info}
+      </InfoModal>
+    </>
   );
 };
 
